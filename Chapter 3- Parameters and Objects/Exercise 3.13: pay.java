5cc68c5c31e5d193a67f1b65e7bc160a369e0b74
LegendaryFoxFire <93889468+LegendaryFoxFire@users.noreply.github.com>
2021-11-07 17:58:12 -0600

    Shorter potential option c:
    Shorter code, capitalizes on importance of Math.min/max methods.

--- Chapter 3- Parameters and Objects/Exercise 3.13: pay.java
+++ Chapter 3- Parameters and Objects/Exercise 3.13: pay.java
@@ -4,10 +4,8 @@ this week, and returns how much money to pay the TA. For example, the call pay(5
 The TA should receive "overtime" pay of 1 ½ normal salary for any hours above 8. For example, the call pay(4.00, 11) should 
 return (4.00 * 8) + (6.00 * 3) or 50.0.*/
 
-public static double pay(double salary, int hours) {
-    if(hours <= 8) {
-        return salary * hours;
-    } else {
-        return (salary * 8) + (salary * 1.5) * (hours - 8);
-    }   
+public static double pay(double rate, int hours) {
+    double pay = rate * Math.min(hours, 8);
+    pay += (rate * 1.5) * Math.max((hours - 8), 0);
+    return pay;
 }
